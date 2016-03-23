b760d97404dd62979a911cdde83ed93906e67dea
nilswende <s142075@student.dhbw-mannheim.de>
2016-03-23 23:36:49 +0100

    finished update time
    updates rly?

---
+++ MagicMirror/js/controller.js
@@ -0,0 +1,19 @@
+﻿
+$(document).ready(function () {
+    moment.locale(locale);
+
+    //only for de-locale
+    (function updateTime() {
+        var now = moment().format("LLLL");
+        var lastSpaceIndex = now.lastIndexOf(" ");
+        var date = now.substring(0, lastSpaceIndex);
+        var clock = now.substring(lastSpaceIndex + 1);
+
+        $(".clock").text(clock);
+        $(".date").text(date);
+
+        setTimeout(function () {
+            updateTime();
+        }, 1000);
+    })();
+});
