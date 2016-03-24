d52ad73c4f76b8012c592b9fbd965cafabfdfbb5
nilswende <s142075@student.dhbw-mannheim.de>
2016-03-24 13:32:36 +0100

    sprit fertig, wetter angefangen

--- MagicMirror/js/controller.js
+++ MagicMirror/js/controller.js
@@ -1,19 +1,96 @@
 ﻿
 $(document).ready(function () {
-    moment.locale(locale);
+	// set moment.js to desired locale
+	moment.locale(locale);
 
-    //only for de-locale
-    (function updateTime() {
-        var now = moment().format("LLLL");
-        var lastSpaceIndex = now.lastIndexOf(" ");
-        var date = now.substring(0, lastSpaceIndex);
-        var clock = now.substring(lastSpaceIndex + 1);
+	(function updateTime() {
+		var now = moment().format("LLLL").split(" ", 4);
+		var date = now[0] + " " + now[1] + " " + now[2];
+		if (showDateWithYear) {
+			date += " " + now[3];
+		}
 
-        $(".clock").text(clock);
-        $(".date").text(date);
+		var clock;
+		if (showClockWithSeconds) {
+			clock = moment().format("LTS");
+		}
+		else {
+			clock = moment().format("LT");
+		}
 
-        setTimeout(function () {
-            updateTime();
-        }, 1000);
-    })();
+		$(".clock").text(clock);
+		$(".date").text(date);
+
+		setTimeout(function () {
+			updateTime();
+		}, 1 * 1000);
+	})();
+
+	station_ids_string = JSON.stringify([stationID]);
+	(function getGasPrice() {
+		$.ajax({
+			url: "https://creativecommons.tankerkoenig.de/json/prices.php",
+			data: {
+				ids: station_ids_string,
+				apikey: tankerkoenigAPIKey
+			},
+			success: function (response) {
+				if (response.ok) {
+					showNewGasPrice(response.prices);
+				} else {
+					showErrorMsgIfNeeded(false);
+				}
+			},
+			error: function (p) {
+				showErrorMsgIfNeeded(true);
+			}
+		});
+		
+		function showNewGasPrice(prices) {
+			var command = "prices[stationID]." + gasType;
+			currentPrice = eval(command);
+			$(".price").text(currentPrice + " €");
+		};
+
+		function showErrorMsgIfNeeded(ajaxError) {
+			if ($(".price").text() == "") {
+				if (ajaxError) {
+					$(".price").text("AJAX error");
+				}
+				else {
+					$(".price").text("bad response");
+				}
+			}
+			/*else keep showing the last price*/
+		};
+
+		setTimeout(function () {
+			getGasPrice();
+		}, 5 * 60 * 1000);
+	})();
+
+	(function getCurrentWeather() {
+		$.ajax({
+			url: "http://api.openweathermap.org/data/2.5/weather",
+			data: {
+				id: cityID,
+				lang: locale,
+				units: "metric",
+				appid: openWeatherMapAPIKey
+			},
+			success: function (response) {
+				var weatherID = response.coord.weather.id;
+				var iconHtml = "<i class=\"wi wi-owm-" + weatherID + "\"></i>";
+				$(".currentWeather").html(iconHtml)
+			},
+			error: function (p) {
+				/*keep showing the last weather*/
+			}
+		});
+
+		setTimeout(function () {
+			getGasPrice();
+		}, 5 * 60 * 1000);
+	})();
+
 });
