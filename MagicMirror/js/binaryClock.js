af03fb611992298f567c14a55a7b05f2c70f1e60
nilswende <s142075@student.dhbw-mannheim.de>
2016-03-30 16:20:13 +0200

    renamed fct

--- MagicMirror/js/binaryClock.js
+++ MagicMirror/js/binaryClock.js
@@ -3,19 +3,19 @@
 
 	var bin;
 	if (time.showClockWithSeconds) {
-		bin = leftpad(now.seconds.toString(2));
+		bin = toSixBit(now.seconds.toString(2));
 		setBgColors(".binSec", bin);
 	}
-	bin = leftpad(now.minutes.toString(2));
+	bin = toSixBit(now.minutes.toString(2));
 	setBgColors(".binMin", bin);
-	bin = leftpad(now.hours.toString(2));
+	bin = toSixBit(now.hours.toString(2));
 	setBgColors(".binHour", bin);
 
 	setTimeout(updateBinaryClock, 0.1 * 1000);
 }
 
-//leftpads with 0s to a length of 6
-function leftpad(str) {
+//leftpad str with 0s to a length of 6
+function toSixBit(str) {
 	return "000000".substring(str.length) + str;
 }
 
@@ -35,15 +35,15 @@ function updateBinaryClock() {
 
 	var bin;
 	if (time.showClockWithSeconds) {
-		bin = leftpad(now.seconds.toString(2));
+		bin = toSixBit(now.seconds.toString(2));
 		setBgColors(".binSec", bin);
 	}
 	if (now.seconds == 0) {
-		bin = leftpad(now.minutes.toString(2));
+		bin = toSixBit(now.minutes.toString(2));
 		setBgColors(".binMin", bin);
 	}
 	if (now.minutes == 0) {
-		bin = leftpad(now.hours.toString(2));
+		bin = toSixBit(now.hours.toString(2));
 		setBgColors(".binHour", bin);
 	}
 
