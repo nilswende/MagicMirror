c4e55daed99824540cd5b88c23f10e6ff7876747
nilswende <s142075@student.dhbw-mannheim.de>
2018-08-10 15:53:46 +0200

    specify http port as flag

--- main/main.go
+++ main/main.go
@@ -4,9 +4,13 @@ import (
 	"log"
 	"net/http"
 	"github.com/nilswende/MagicMirror-server/serverlib"
+	"flag"
+	"fmt"
 )
 
 func main() {
+	port := flag.Int("port", 8080, "The port to listen on")
+	flag.Parse()
 	http.HandleFunc("/", serverlib.NewHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
